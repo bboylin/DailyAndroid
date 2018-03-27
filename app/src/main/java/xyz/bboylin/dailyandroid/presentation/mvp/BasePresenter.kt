65c6c9330312166e93920e2f4183d39d2af71c66
bboylin <bboylin24@gmail.com>
2018-03-27 16:38:43 +0800

    重构成clean-mvp架构

---
+++ app/src/main/java/xyz/bboylin/dailyandroid/presentation/mvp/BasePresenter.kt
@@ -0,0 +1,8 @@
+package xyz.bboylin.dailyandroid.presentation.mvp
+
+/**
+ * @author lin on 2018/3/27.
+ * email: bboylin24@gmail.com
+ */
+class BasePresenter {
+}
---
+++ app/src/main/java/xyz/bboylin/dailyandroid/presentation/mvp/contract/WeeklyContract.kt
@@ -0,0 +1,8 @@
+package xyz.bboylin.dailyandroid.presentation.mvp.contract
+
+/**
+ * @author lin on 2018/3/27.
+ * email: bboylin24@gmail.com
+ */
+class WeeklyContract {
+}
